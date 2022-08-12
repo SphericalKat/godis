6247debdbdf2c5538a15cab98c944b66b6892d7a
Amogh Lele <amolele@gmail.com>
2022-08-12 22:56:13 +0530

    feat: implement initial connection handling
    Signed-off-by: Amogh Lele <amolele@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	// listen on port 6379
+	lis, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		log.Fatal("something went wrong:", err)
+		os.Exit(1)
+	}
+
+	defer lis.Close()
+
+	log.Println("listening for connections on port 6379")
+
+	// start accepting incoming connections
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		log.Println("got connection:", conn.RemoteAddr())
+		go handleIncomingRequest(conn)
+	}
+}
+
+func handleIncomingRequest(conn net.Conn) {
+	// store incoming data
+	buffer := make([]byte, 1024)
+	_, err := conn.Read(buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// respond
+	conn.Write([]byte("Hi back!\n"))
+
+	// close conn
+	conn.Close()
 }
